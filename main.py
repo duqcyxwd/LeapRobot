589e1bd9ea7acb7e7ab8e568d25d750546f73085
Yongqinchuan Du <duqcyxwd@gmail.com>
2015-01-14 13:37:11 -0500

    fix control page command

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from PyQt5.uic import loadUi
 
 from controlPage import ControlPage
 
-class DemoImpl(QMainWindow):
+class MainWindow(QMainWindow):
 	def __init__(self, *args):
-		super(DemoImpl, self).__init__(*args)
+		super(MainWindow, self).__init__(*args)
 
 		loadUi('MainWindow.ui', self)
 		self.actionTesting.triggered.connect(self.testing)
 		self.actionAbout_us.triggered.connect(self.openAbout)
+
+		self._controlPage = "null"
 
 
 	def testing(self):
@@ -24,29 +26,22 @@ class DemoImpl(QMainWindow):
 
 		message = "This is Leap-Robotic control program."
 		reply = QMessageBox.information(self,
-		        "QMessageBox.information()", message)
+				"QMessageBox.information()", message)
 
 	@pyqtSlot()
 	def on_newtab_clicked(self):
-		print "open control page";
+		print "open/focus on control page";
 
-		widge = QWidget()
-		# if graph != "null":
-		#     self._graphControlWidget.setGraph(graph)
-		#     widgetName = graph.plotType
-		# else:
+		if self._controlPage == "null":
+			widgetName = "control"
+			self._controlPage = ControlPage(self)
+			self.tabWidget.addTab(self._controlPage, widgetName)
 
-		widgetName = "control"
-
-		self._controlPage = ControlPage(self)
-		self.tabWidget.addTab(self._controlPage, widgetName)
 		self.tabWidget.setCurrentWidget(self._controlPage)
-
-
 
 if __name__ == '__main__':
 		
 	app = QApplication(sys.argv)
-	widget = DemoImpl()
+	widget = MainWindow()
 	widget.show()
 	sys.exit(app.exec_())
